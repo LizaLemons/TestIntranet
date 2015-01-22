d8d93f80bc1879c2738b4f7a1e33ea14464fa439
Liza Ramo <liza.h.ramo@gmail.com>
2015-01-22 17:20:34 -0500

    several changes to mockup3

--- Mockup3/mockup3.js
+++ Mockup3/mockup3.js
@@ -1,31 +1,39 @@
 
+
+// Set margin-top of main-and-footer-container equal to the height of the header/nav
+var heightOfNavbar = $("#header-navbar").height();
+$("#main-and-footer-container").css({marginTop: heightOfNavbar + -1 + 'px'});
+
+// Set margin-bottom of main-and-footer-container equal to the height of the footer
+var heightOfFooter = $("#footer-navbar").height();
+$("#main-and-footer-container").css({marginBottom: heightOfFooter + -15 + 'px'});
+
 // Mark links in navbar as active
 $(".nav a").on("click", function(){
 	$(".nav").find(".active").removeClass("active");
 	$(this).parent().addClass("active");
 });
 
-// Set margin-top of contentBox to be the height of the header/nav + 30
-var heightOfNavbar = $("#navBar").height();
-$("#contentBox").css({marginTop: heightOfNavbar + 30 + 'px'});
-console.log($("#contentBox").css("margin-top"));
-
 // Fade nav links by adding class 'fade'
-$('#navbar li').mouseover(function() {
-	$(this).siblings().addClass("fade");
-}).mouseout(function() {
-	$(this).siblings().removeClass("fade");
-});
+// Only main nav, not dropdown
+//$('#navbar > ul > li').mouseover(function() {
+//	$(this).siblings().addClass("fade");
+//}).mouseout(function() {
+//	$(this).siblings().removeClass("fade");
+//});
+
+// Underline active & hover nav links
 
 
-// Make border-bottom appear/disappear on scroll
+
+// Make bottom shadow of header appear/disappear on scroll
 $(window).scroll(function() {
 	var value = $(window).scrollTop();
 	if ( value > 0 ) {
-		$('#header-nav-container').addClass('header-bottom-border');
+		$('#add-shadow').addClass('header-bottom-border');
 	}
 	else {
-		$('#header-nav-container').removeClass('header-bottom-border');
+		$('#add-shadow').removeClass('header-bottom-border');
 	}
 });
 
